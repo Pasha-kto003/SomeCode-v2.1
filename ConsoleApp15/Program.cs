878ec15689a1e9ed95f2e45ce2901922daf4dcfc
Паша Луковников <lukovnikov.pasha@gmail.com>
2021-08-19 16:22:15 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a, b, c;
+            //a = int.Parse(Console.ReadLine());
+            //b = int.Parse(Console.ReadLine());
+            //c = Summa(a, b);
+            //Summ(c);
+            //Multy(a, b);
+            //AddPerson();
+            //Console.WriteLine("Выберите тот элемент который хотите ввести");
+            //string symbol = Console.ReadLine();
+            //Console.WriteLine("Количество элементов");
+            //int count = int.Parse(Console.ReadLine());
+            //Print(symbol, count);
+            int[] array = { 2, 7, 8, 10, 12 };
+            int res = FindElement(array, 10);
+            Console.WriteLine(res);
+        }
+
+        //static void Stroka()
+        //{
+        //    Console.WriteLine("Выберите тот элемент который хотите ввести");
+        //    string element = Console.ReadLine();
+        //    Console.WriteLine($"Сколько раз вы  хотите ввести элемент: {element}?");
+        //    int value = int.Parse(Console.ReadLine());
+        //    Console.WriteLine($"Вводите элемент {element}, {value} раз");
+        //    string text = Console.ReadLine();
+        //    if(text!.Equals(element))
+        //    {
+        //        Console.WriteLine("Вы ввели не тот символ");          
+        //    }
+        //}
+        //static void Summ(int result)
+        //{
+        //    Console.WriteLine("Рузультат суммы" + result);
+        //}
+        //static int Summa(int x, int y)
+        //{
+        //    int c = x + y;
+        //    Console.WriteLine(c);
+        //    return c;
+        //}
+        //static int Multy(int x, int y)
+        //{
+        //    int c = x * y;
+        //    Console.WriteLine(c);
+        //    return c;
+        //}
+        //static void AddPerson()
+        //{
+        //    string name = Console.ReadLine();
+        //    int age = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Name: " + name + " Age: " + age);
+        //}
+
+        //static void Print(string message, int count)
+        //{  
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        Console.Write(message);
+        //    }
+        //}
+
+        static int FindElement(int[] massive, int value)
+        {
+            for (int i = 0; i < massive.Length; i++)
+            {
+                if(massive[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
